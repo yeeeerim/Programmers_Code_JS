745aef4e64df5c131126d5b0a27ecd7933352fd2
yerim <pppp5097@ynu.ac.kr>
2022-03-06 18:05:32 +0900

    신규 아이디 추천

---
+++ LV1/003.js
@@ -0,0 +1,9 @@
+// 신규 아이디 추천
+// https://programmers.co.kr/learn/courses/30/lessons/72410?language=javascript
+
+function solution(new_id) {
+  var answer = "";
+  return answer;
+}
+
+console.log("====== 정답 ======> " + solution("...!@BaT#*..y.abcdefghijklm"));
